74221c6883f0cbceb67a23e6c70caa43a8683279
RubenRguezT <rucho_88@hotmail.es>
2025-05-04 22:41:20 +0200

    Add files via upload

---
+++ Eliminar_libro_bd.php
@@ -0,0 +1,100 @@
+<?php 
+include_once ('funciones_bd.php');
+$consulta = eliminar_libro();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Eliminar Libro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+
+        <div class="container-fluid">
+            <div class="w-75 mx-auto">
+                <span class="navbar-text">
+                    <h3 class="text-white fw-bold">Bienvenido a Libreria Ruben</h3>
+                </span>
+            </div>
+            <a class="navbar-brand" href="Inicio.php"><strong>Inicio</strong></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="Consulta_total_libros.php"><strong>Todos los Libros</strong></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><strong>Generos</strong></a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="genero_accion.php">Accion</a></li>
+                            <li><a class="dropdown-item" href="genero_ciencia_ficcion.php">Ciencia Ficcion</a></li>
+                            <li><a class="dropdown-item" href="genero_comedia.php">Comedia</a></li>
+                            <li><a class="dropdown-item" href="genero_drama.php">Drama</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </br>
+    </br>
+
+    <div class="container">
+        <div class="d-flex justify-content-center gap-3">
+            <a href="form_nuevo_libro.html" class="btn btn-dark">Añadir Libro</a>
+            <a href="form_eliminar_libro.html" class="btn btn-dark">Eliminar Libro</a>
+            <a href="form_modificar_libro.html" class="btn btn-dark">Modificar Libro</a>
+        </div></br>
+
+        <h1>Libro eliminado correctamente</h1></br>
+
+        <!-- creamos la tabla !-->
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Año</th>
+                    <th scope="col">Genero</th>
+                    <th scope="col">Resumen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                // Realizamos la consulta de los libros que continuan en la base de datos
+
+                
+                $cont = 0;
+
+                while ($fila = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+                    $libro[$cont++] = $fila;
+                    echo '<tr>';
+                    echo "<td>{$fila['id']}</td>";
+                    echo "<td>{$fila['isbn']}</td>";
+                    echo "<td>{$fila['titulo']}</td>";
+                    echo "<td>{$fila['autor']}</td>";
+                    echo "<td>{$fila['anio']}</td>";
+                    echo "<td>{$fila['genero']}</td>";
+                    echo "<td>{$fila['resumen']}</td>";
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ funciones_bd.php
@@ -0,0 +1,351 @@
+<?php
+
+// Funcion que utilizamos para obtener los 4 primeros libros de la tabla para la página de inicio.
+
+function obter_os_catro_primerios_libros()
+{
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos de los 5 primeros libros de la tabla
+
+    $consulta = mysqli_query($conexion, "select * from libros order by id asc limit 4;");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+// Funcion que utilizamos para obtener la tabla con todos los libros.
+
+function total_libros()
+{
+
+  // conectamos con la base de datos,
+
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos 
+    $consulta = mysqli_query($conexion, "select * from libros");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+// Funcion que utilizamos para obtener la tabla con todos los libros del genero accion.
+
+
+function genero_accion()
+{
+  // conectamos con la base de datos,
+
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos de los libros con el genero accion
+    $consulta = mysqli_query($conexion, "select * from libros where genero = 'accion'");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+  return $consulta;
+}
+
+
+
+// Funcion que utilizamos para obtener la tabla con todos los libros del genero ciencia ficcion.
+
+function genero_ciencia_ficcion()
+{
+
+  // conectamos con la base de datos,
+
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos de los libros con el genero Ciencia Ficcion
+    $consulta = mysqli_query($conexion, "select * from libros where genero = 'Ciencia Ficcion'");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+
+// Funcion que utilizamos para obtener la tabla con todos los libros del genero Comedia.
+
+function genero_comedia()
+{
+
+  // conectamos con la base de datos,
+
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos de los libros con el genero comedia
+    $consulta = mysqli_query($conexion, "select * from libros where genero = 'comedia'");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+  return $consulta;
+}
+
+
+
+// Funcion que utilizamos para obtener la tabla con todos los libros del genero drama.
+
+
+function genero_drama()
+{
+  // conectamos con la base de datos,
+
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // Realizamos la consulta a la base de datos de los libros con el genero drama
+    $consulta = mysqli_query($conexion, "select * from libros where genero = 'drama'");
+
+
+    // configuramos el mensaje de error
+
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+// Funcion que utilizamos para añadir un libro.
+
+function insertar_libro()
+{
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // obtenemos los datos
+
+    $isbn = $_POST["isbn"];
+    $titulo = $_POST["titulo"];
+    $autor = $_POST["autor"];
+    $anio = $_POST['anio'];
+    $genero = $_POST["genero"];
+    $resumen = $_POST["resumen"];
+
+    // Realizamos la insercion en nuestra tabla
+
+    $insert = "INSERT INTO libros (isbn, titulo, autor, anio, genero, resumen) VALUES ('$isbn', '$titulo', '$autor', $anio, '$genero', '$resumen')";
+
+    // Ejecutamos la insercion
+    mysqli_query($conexion, $insert);
+
+    // Realizamos la consulta a la base de datos del libro añadido la tabla
+    $consulta = mysqli_query($conexion, "select * from libros where isbn = $isbn");
+
+    // configuramos el mensaje de error
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+// Funcion que utilizamos para eliminar un libro.
+
+function eliminar_libro()
+{
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // obtenemos los datos
+
+    $id = $_POST["id"];
+
+    // Realizamos la eliminacion del libro de nuestra tabla
+
+    $eliminar = "DELETE FROM libros WHERE id = $id";
+
+    mysqli_query($conexion, $eliminar);
+
+    $consulta = mysqli_query($conexion, "SELECT * FROM libros");
+
+    // configuramos el mensaje de error
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
+
+
+// Funciona para obtener el formulario del libro y poder modificarlo
+
+function modificar_libro()
+{
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // obtenemos los datos para poder modificarlos
+
+    $id = $_POST["id"];
+
+
+    $consulta = "SELECT * FROM libros WHERE id = $id";
+
+
+    // Seleccionamos los datos 
+
+    $resultado = mysqli_query($conexion, $consulta);
+
+    // configuramos el mensaje de error
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $resultado;
+}
+
+
+// Funciona para mostrar el resultado del libro modificado.
+
+function resultado_modificacion()
+{
+  $host =  "localhost";
+  $user =  "librouser";
+  $pwd = "passworduser";
+  $database = "Ruben_Rguez_BD";
+
+  try {
+    // conectamos a base de datos
+    $conexion = mysqli_connect($host, $user, $pwd);
+
+    mysqli_select_db($conexion, $database);
+
+    // obtenemos los datos para poder modificarlos
+
+    $id = $_POST["id"];
+    $isbn = $_POST["isbn"];
+    $titulo = $_POST["titulo"];
+    $autor = $_POST["autor"];
+    $anio = $_POST["anio"];
+    $genero = $_POST["genero"];
+    $resumen = $_POST["resumen"];
+
+    // realizamos la modificacion de los datos
+
+    $modificar = "UPDATE libros SET id='$id', isbn='$isbn', titulo='$titulo',autor='$autor', anio='$anio', genero='$genero',resumen='$resumen' WHERE id=$id";
+
+    mysqli_query($conexion, $modificar);
+
+    // configuramos la consulta
+
+    $consulta = mysqli_query($conexion,"SELECT * FROM libros WHERE id = $id");
+
+
+    // configuramos el mensaje de error
+  } catch (Exception $e) {
+    echo 'Acaba de ocorrer un erro :' . $e->getMessage();
+  }
+
+  return $consulta;
+}
